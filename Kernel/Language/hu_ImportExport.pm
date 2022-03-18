12b6b0605f97868b520973f40cf222f271caccd6
Konrad Gumienny <kgumienny@opgksoftware.com>
2022-03-18 15:47:35 +0100

    Package ported to Znuny 6.3.

--- Kernel/Language/hu_ImportExport.pm
+++ Kernel/Language/hu_ImportExport.pm
@@ -1,6 +1,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-# Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021-2022 Znuny GmbH, https://znuny.org/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,62 +18,75 @@ sub Data {
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'Importálás/exportálás kezelés';
-    $Self->{Translation}->{'Add template'} = 'Sablon hozzáadása';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Sablon létrehozása objektuminformációk importálásához és exportálásához.';
-    $Self->{Translation}->{'To use this module, you need to install ITSMConfigurationManagement or any other package that provides back end for objects to be imported and exported.'} =
+    $Self->{Translation}->{'Add template'}             = 'Sablon hozzáadása';
+    $Self->{Translation}->{'Create a template to import and export object information.'}
+        = 'Sablon létrehozása objektuminformációk importálásához és exportálásához.';
+    $Self->{Translation}->{
+        'To use this module, you need to install ITSMConfigurationManagement or any other package that provides back end for objects to be imported and exported.'
+        } =
         'A modul használatához telepítenie kell az ITSMConfigurationManagement csomagot vagy bármilyen egyéb csomagot, amely háttérprogramot biztosít az objektumok importálásához és exportálásához.';
-    $Self->{Translation}->{'Start Import'} = 'Importálás indítása';
-    $Self->{Translation}->{'Start Export'} = 'Exportálás indítása';
+    $Self->{Translation}->{'Start Import'}         = 'Importálás indítása';
+    $Self->{Translation}->{'Start Export'}         = 'Exportálás indítása';
     $Self->{Translation}->{'Delete this template'} = 'Sablon törlése';
-    $Self->{Translation}->{'Step 1 of 5 - Edit common information'} = '1. lépés az 5-ből - közös információk szerkesztése';
-    $Self->{Translation}->{'Name is required!'} = 'A név kötelező!';
+    $Self->{Translation}->{'Step 1 of 5 - Edit common information'}
+        = '1. lépés az 5-ből - közös információk szerkesztése';
+    $Self->{Translation}->{'Name is required!'}   = 'A név kötelező!';
     $Self->{Translation}->{'Object is required!'} = 'Az objektum kötelező!';
     $Self->{Translation}->{'Format is required!'} = 'A formátum kötelező!';
-    $Self->{Translation}->{'Step 2 of 5 - Edit object information'} = '2. lépés az 5-ből - objektuminformációk szerkesztése';
-    $Self->{Translation}->{'Step 3 of 5 - Edit format information'} = '3. lépés az 5-ből - formátuminformációk szerkesztése';
+    $Self->{Translation}->{'Step 2 of 5 - Edit object information'}
+        = '2. lépés az 5-ből - objektuminformációk szerkesztése';
+    $Self->{Translation}->{'Step 3 of 5 - Edit format information'}
+        = '3. lépés az 5-ből - formátuminformációk szerkesztése';
     $Self->{Translation}->{'is required!'} = 'kötelező!';
-    $Self->{Translation}->{'Step 4 of 5 - Edit mapping information'} = '4. lépés az 5-ből - leképezési információk szerkesztése';
+    $Self->{Translation}->{'Step 4 of 5 - Edit mapping information'}
+        = '4. lépés az 5-ből - leképezési információk szerkesztése';
     $Self->{Translation}->{'No map elements found.'} = 'Nem találhatók leképezési elemek.';
-    $Self->{Translation}->{'Add Mapping Element'} = 'Leképezési elem hozzáadása';
-    $Self->{Translation}->{'Step 5 of 5 - Edit search information'} = '5. lépés az 5-ből - keresési információk szerkesztése';
+    $Self->{Translation}->{'Add Mapping Element'}    = 'Leképezési elem hozzáadása';
+    $Self->{Translation}->{'Step 5 of 5 - Edit search information'}
+        = '5. lépés az 5-ből - keresési információk szerkesztése';
     $Self->{Translation}->{'Restrict export per search'} = 'Exportálás korlátozása keresésenként';
-    $Self->{Translation}->{'Import information'} = 'Importálási információk';
-    $Self->{Translation}->{'Source File'} = 'Forrásfájl';
-    $Self->{Translation}->{'Import summary for %s'} = '%s importálási összegzése';
-    $Self->{Translation}->{'Records'} = 'Rekordok';
-    $Self->{Translation}->{'Success'} = 'Sikeres';
-    $Self->{Translation}->{'Duplicate names'} = 'Nevek kettőzése';
-    $Self->{Translation}->{'Last processed line number of import file'} = 'Az importfájl utolsó feldolgozott sorszáma';
+    $Self->{Translation}->{'Import information'}         = 'Importálási információk';
+    $Self->{Translation}->{'Source File'}                = 'Forrásfájl';
+    $Self->{Translation}->{'Import summary for %s'}      = '%s importálási összegzése';
+    $Self->{Translation}->{'Records'}                    = 'Rekordok';
+    $Self->{Translation}->{'Success'}                    = 'Sikeres';
+    $Self->{Translation}->{'Duplicate names'}            = 'Nevek kettőzése';
+    $Self->{Translation}->{'Last processed line number of import file'}
+        = 'Az importfájl utolsó feldolgozott sorszáma';
     $Self->{Translation}->{'Ok'} = 'OK';
-    $Self->{Translation}->{'Do you really want to delete this template item?'} = 'Valóban törölni szeretné ezt a sablonelemet?';
+    $Self->{Translation}->{'Do you really want to delete this template item?'}
+        = 'Valóban törölni szeretné ezt a sablonelemet?';
 
     # Perl Module: Kernel/Modules/AdminImportExport.pm
-    $Self->{Translation}->{'No object backend found!'} = 'Nem található objektum-háttérprogram!';
-    $Self->{Translation}->{'No format backend found!'} = 'Nem található formátum-háttérprogram!';
-    $Self->{Translation}->{'Template not found!'} = 'Sablon nem található!';
+    $Self->{Translation}->{'No object backend found!'}       = 'Nem található objektum-háttérprogram!';
+    $Self->{Translation}->{'No format backend found!'}       = 'Nem található formátum-háttérprogram!';
+    $Self->{Translation}->{'Template not found!'}            = 'Sablon nem található!';
     $Self->{Translation}->{'Can\'t insert/update template!'} = 'Nem lehet beszúrni vagy frissíteni a sablont!';
-    $Self->{Translation}->{'Needed TemplateID!'} = 'Sablon-azonosító szükséges!';
-    $Self->{Translation}->{'Error occurred. Import impossible! See Syslog for details.'} = 'Hiba történt. Az importálás lehetetlen! Nézze meg a rendszernaplót a részletekért.';
-    $Self->{Translation}->{'Error occurred. Export impossible! See Syslog for details.'} = 'Hiba történt. Az exportálás lehetetlen! Nézze meg a rendszernaplót a részletekért.';
-    $Self->{Translation}->{'Template List'} = 'Sablonlista';
-    $Self->{Translation}->{'number'} = 'szám';
-    $Self->{Translation}->{'number bigger than zero'} = 'nullánál nagyobb szám';
-    $Self->{Translation}->{'integer'} = 'egész';
-    $Self->{Translation}->{'integer bigger than zero'} = 'nullánál nagyobb egész';
+    $Self->{Translation}->{'Needed TemplateID!'}             = 'Sablon-azonosító szükséges!';
+    $Self->{Translation}->{'Error occurred. Import impossible! See Syslog for details.'}
+        = 'Hiba történt. Az importálás lehetetlen! Nézze meg a rendszernaplót a részletekért.';
+    $Self->{Translation}->{'Error occurred. Export impossible! See Syslog for details.'}
+        = 'Hiba történt. Az exportálás lehetetlen! Nézze meg a rendszernaplót a részletekért.';
+    $Self->{Translation}->{'Template List'}                        = 'Sablonlista';
+    $Self->{Translation}->{'number'}                               = 'szám';
+    $Self->{Translation}->{'number bigger than zero'}              = 'nullánál nagyobb szám';
+    $Self->{Translation}->{'integer'}                              = 'egész';
+    $Self->{Translation}->{'integer bigger than zero'}             = 'nullánál nagyobb egész';
     $Self->{Translation}->{'Element required, please insert data'} = 'Elem szükséges, szúrjon be adatokat';
-    $Self->{Translation}->{'Invalid data, please insert a valid %s'} = 'Érvénytelen adatok, egy érvényes %s beszúrása szükséges';
+    $Self->{Translation}->{'Invalid data, please insert a valid %s'}
+        = 'Érvénytelen adatok, egy érvényes %s beszúrása szükséges';
     $Self->{Translation}->{'Format not found!'} = 'Formátum nem található!';
 
     # Perl Module: Kernel/System/ImportExport/FormatBackend/CSV.pm
-    $Self->{Translation}->{'Column Separator'} = 'Oszlopelválasztó';
-    $Self->{Translation}->{'Tabulator (TAB)'} = 'Tabulátor (TAB)';
-    $Self->{Translation}->{'Semicolon (;)'} = 'Pontosvessző (;)';
-    $Self->{Translation}->{'Colon (:)'} = 'Kettőspont (:)';
-    $Self->{Translation}->{'Dot (.)'} = 'Pont (.)';
-    $Self->{Translation}->{'Comma (,)'} = 'Vessző (,)';
-    $Self->{Translation}->{'Charset'} = 'Karakterkészlet';
+    $Self->{Translation}->{'Column Separator'}       = 'Oszlopelválasztó';
+    $Self->{Translation}->{'Tabulator (TAB)'}        = 'Tabulátor (TAB)';
+    $Self->{Translation}->{'Semicolon (;)'}          = 'Pontosvessző (;)';
+    $Self->{Translation}->{'Colon (:)'}              = 'Kettőspont (:)';
+    $Self->{Translation}->{'Dot (.)'}                = 'Pont (.)';
+    $Self->{Translation}->{'Comma (,)'}              = 'Vessző (,)';
+    $Self->{Translation}->{'Charset'}                = 'Karakterkészlet';
     $Self->{Translation}->{'Include Column Headers'} = 'Oszlopfejlécek felvétele';
-    $Self->{Translation}->{'Column'} = 'Oszlop';
+    $Self->{Translation}->{'Column'}                 = 'Oszlop';
 
     # JS File: ITSM.Admin.ImportExport
     $Self->{Translation}->{'Deleting template...'} = 'Sablon törlése…';
@@ -84,18 +97,20 @@ sub Data {
     # SysConfig
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
         'Formátum háttérprogram modul regisztráció az importálás/exportálás modulhoz.';
-    $Self->{Translation}->{'Import and export object information.'} = 'Objektuminformációk importálása és exportálása.';
+    $Self->{Translation}->{'Import and export object information.'}
+        = 'Objektuminformációk importálása és exportálása.';
     $Self->{Translation}->{'Import/Export'} = 'Importálás/exportálás';
 
-
     push @{ $Self->{JavaScriptStrings} // [] }, (
-    'Cancel',
-    'Confirm',
-    'Delete this template',
-    'Deleting template...',
-    'Template was deleted successfully.',
-    'There was an error deleting the template. Please check the logs for more information.',
+        'Cancel',
+        'Confirm',
+        'Delete this template',
+        'Deleting template...',
+        'Template was deleted successfully.',
+        'There was an error deleting the template. Please check the logs for more information.',
     );
+
+    return 1;
 
 }
 
--- Kernel/Language/id_ImportExport.pm
+++ Kernel/Language/id_ImportExport.pm
@@ -1,6 +1,6 @@
 # --
 # Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
-# Copyright (C) 2021 Znuny GmbH, https://znuny.org/
+# Copyright (C) 2021-2022 Znuny GmbH, https://znuny.org/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
@@ -18,62 +18,66 @@ sub Data {
 
     # Template: AdminImportExport
     $Self->{Translation}->{'Import/Export Management'} = 'Manajamen Impor/Ekspor';
-    $Self->{Translation}->{'Add template'} = 'Tambahkan templat';
-    $Self->{Translation}->{'Create a template to import and export object information.'} = 'Buat sebuah template untuk mengimpor dan ekspor obyek informasi.';
-    $Self->{Translation}->{'To use this module, you need to install ITSMConfigurationManagement or any other package that provides back end for objects to be imported and exported.'} =
+    $Self->{Translation}->{'Add template'}             = 'Tambahkan templat';
+    $Self->{Translation}->{'Create a template to import and export object information.'}
+        = 'Buat sebuah template untuk mengimpor dan ekspor obyek informasi.';
+    $Self->{Translation}->{
+        'To use this module, you need to install ITSMConfigurationManagement or any other package that provides back end for objects to be imported and exported.'
+        } =
         '';
-    $Self->{Translation}->{'Start Import'} = 'Mulai Impor';
-    $Self->{Translation}->{'Start Export'} = 'Mulai Ekspor';
-    $Self->{Translation}->{'Delete this template'} = '';
-    $Self->{Translation}->{'Step 1 of 5 - Edit common information'} = '';
-    $Self->{Translation}->{'Name is required!'} = 'Nama dibutuhkan!';
-    $Self->{Translation}->{'Object is required!'} = 'Obyek dibutuhkan!';
-    $Self->{Translation}->{'Format is required!'} = 'Format dibutuhkan!';
-    $Self->{Translation}->{'Step 2 of 5 - Edit object information'} = '';
-    $Self->{Translation}->{'Step 3 of 5 - Edit format information'} = '';
-    $Self->{Translation}->{'is required!'} = 'dibutuhkan!';
+    $Self->{Translation}->{'Start Import'}                           = 'Mulai Impor';
+    $Self->{Translation}->{'Start Export'}                           = 'Mulai Ekspor';
+    $Self->{Translation}->{'Delete this template'}                   = '';
+    $Self->{Translation}->{'Step 1 of 5 - Edit common information'}  = '';
+    $Self->{Translation}->{'Name is required!'}                      = 'Nama dibutuhkan!';
+    $Self->{Translation}->{'Object is required!'}                    = 'Obyek dibutuhkan!';
+    $Self->{Translation}->{'Format is required!'}                    = 'Format dibutuhkan!';
+    $Self->{Translation}->{'Step 2 of 5 - Edit object information'}  = '';
+    $Self->{Translation}->{'Step 3 of 5 - Edit format information'}  = '';
+    $Self->{Translation}->{'is required!'}                           = 'dibutuhkan!';
     $Self->{Translation}->{'Step 4 of 5 - Edit mapping information'} = '';
-    $Self->{Translation}->{'No map elements found.'} = 'Tidak ditemukan elemen peta.';
-    $Self->{Translation}->{'Add Mapping Element'} = 'Tambah Elemen Pemetaan';
-    $Self->{Translation}->{'Step 5 of 5 - Edit search information'} = '';
-    $Self->{Translation}->{'Restrict export per search'} = 'Batas ekspor per pencarian';
-    $Self->{Translation}->{'Import information'} = 'Informasi impor ';
-    $Self->{Translation}->{'Source File'} = 'Berkas Sumber';
-    $Self->{Translation}->{'Import summary for %s'} = '';
-    $Self->{Translation}->{'Records'} = 'Data';
-    $Self->{Translation}->{'Success'} = 'Berhasil';
-    $Self->{Translation}->{'Duplicate names'} = 'Nama duplikat';
-    $Self->{Translation}->{'Last processed line number of import file'} = 'Nomor baris terakhir berkas impor yang diproses';
-    $Self->{Translation}->{'Ok'} = 'Ok';
+    $Self->{Translation}->{'No map elements found.'}                 = 'Tidak ditemukan elemen peta.';
+    $Self->{Translation}->{'Add Mapping Element'}                    = 'Tambah Elemen Pemetaan';
+    $Self->{Translation}->{'Step 5 of 5 - Edit search information'}  = '';
+    $Self->{Translation}->{'Restrict export per search'}             = 'Batas ekspor per pencarian';
+    $Self->{Translation}->{'Import information'}                     = 'Informasi impor ';
+    $Self->{Translation}->{'Source File'}                            = 'Berkas Sumber';
+    $Self->{Translation}->{'Import summary for %s'}                  = '';
+    $Self->{Translation}->{'Records'}                                = 'Data';
+    $Self->{Translation}->{'Success'}                                = 'Berhasil';
+    $Self->{Translation}->{'Duplicate names'}                        = 'Nama duplikat';
+    $Self->{Translation}->{'Last processed line number of import file'}
+        = 'Nomor baris terakhir berkas impor yang diproses';
+    $Self->{Translation}->{'Ok'}                                               = 'Ok';
     $Self->{Translation}->{'Do you really want to delete this template item?'} = '';
 
     # Perl Module: Kernel/Modules/AdminImportExport.pm
-    $Self->{Translation}->{'No object backend found!'} = '';
-    $Self->{Translation}->{'No format backend found!'} = '';
-    $Self->{Translation}->{'Template not found!'} = '';
-    $Self->{Translation}->{'Can\'t insert/update template!'} = '';
-    $Self->{Translation}->{'Needed TemplateID!'} = '';
+    $Self->{Translation}->{'No object backend found!'}                                   = '';
+    $Self->{Translation}->{'No format backend found!'}                                   = '';
+    $Self->{Translation}->{'Template not found!'}                                        = '';
+    $Self->{Translation}->{'Can\'t insert/update template!'}                             = '';
+    $Self->{Translation}->{'Needed TemplateID!'}                                         = '';
     $Self->{Translation}->{'Error occurred. Import impossible! See Syslog for details.'} = '';
     $Self->{Translation}->{'Error occurred. Export impossible! See Syslog for details.'} = '';
-    $Self->{Translation}->{'Template List'} = '';
-    $Self->{Translation}->{'number'} = '';
-    $Self->{Translation}->{'number bigger than zero'} = '';
-    $Self->{Translation}->{'integer'} = '';
-    $Self->{Translation}->{'integer bigger than zero'} = '';
-    $Self->{Translation}->{'Element required, please insert data'} = '';
-    $Self->{Translation}->{'Invalid data, please insert a valid %s'} = '';
-    $Self->{Translation}->{'Format not found!'} = '';
+    $Self->{Translation}->{'Template List'}                                              = '';
+    $Self->{Translation}->{'number'}                                                     = '';
+    $Self->{Translation}->{'number bigger than zero'}                                    = '';
+    $Self->{Translation}->{'integer'}                                                    = '';
+    $Self->{Translation}->{'integer bigger than zero'}                                   = '';
+    $Self->{Translation}->{'Element required, please insert data'}                       = '';
+    $Self->{Translation}->{'Invalid data, please insert a valid %s'}                     = '';
+    $Self->{Translation}->{'Format not found!'}                                          = '';
 
     # Perl Module: Kernel/System/ImportExport/FormatBackend/CSV.pm
-    $Self->{Translation}->{'Column Separator'} = 'Pemisah Kolom';
-    $Self->{Translation}->{'Tabulator (TAB)'} = 'Tab (TAB)';
-    $Self->{Translation}->{'Semicolon (;)'} = 'Titikkoma (;)';
-    $Self->{Translation}->{'Colon (:)'} = 'Titikdua (:)';
-    $Self->{Translation}->{'Dot (.)'} = 'Titik (.)';
-    $Self->{Translation}->{'Comma (,)'} = '';
-    $Self->{Translation}->{'Charset'} = 'Karakter';
+    $Self->{Translation}->{'Column Separator'}       = 'Pemisah Kolom';
+    $Self->{Translation}->{'Tabulator (TAB)'}        = 'Tab (TAB)';
+    $Self->{Translation}->{'Semicolon (;)'}          = 'Titikkoma (;)';
+    $Self->{Translation}->{'Colon (:)'}              = 'Titikdua (:)';
+    $Self->{Translation}->{'Dot (.)'}                = 'Titik (.)';
+    $Self->{Translation}->{'Comma (,)'}              = '';
+    $Self->{Translation}->{'Charset'}                = 'Karakter';
     $Self->{Translation}->{'Include Column Headers'} = 'Sertakan Kepala Kolom';
-    $Self->{Translation}->{'Column'} = 'Kolom';
+    $Self->{Translation}->{'Column'}                 = 'Kolom';
 
     # JS File: ITSM.Admin.ImportExport
     $Self->{Translation}->{'Deleting template...'} = '';
@@ -85,17 +89,18 @@ sub Data {
     $Self->{Translation}->{'Format backend module registration for the import/export module.'} =
         'Format modul pendaftaran backend untuk modul impor/ekspor.';
     $Self->{Translation}->{'Import and export object information.'} = 'Informasi obyek impor dan ekspor.';
-    $Self->{Translation}->{'Import/Export'} = 'Impor/Ekspor';
-
+    $Self->{Translation}->{'Import/Export'}                         = 'Impor/Ekspor';
 
     push @{ $Self->{JavaScriptStrings} // [] }, (
-    'Cancel',
-    'Confirm',
-    'Delete this template',
-    'Deleting template...',
-    'Template was deleted successfully.',
-    'There was an error deleting the template. Please check the logs for more information.',
+        'Cancel',
+        'Confirm',
+        'Delete this template',
+        'Deleting template...',
+        'Template was deleted successfully.',
+        'There was an error deleting the template. Please check the logs for more information.',
     );
+
+    return 1;
 
 }
 
